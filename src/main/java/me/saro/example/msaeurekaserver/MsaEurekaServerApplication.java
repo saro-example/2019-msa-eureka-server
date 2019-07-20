b8e0903230cac74141b4f5394ea82360f38ab276
PARK Yong Seo <j@saro.me>
2019-07-20 21:07:09 +0900

    append jaxb

--- src/main/java/me/saro/example/msaeurekaserver/MsaEurekaServerApplication.java
+++ src/main/java/me/saro/example/msaeurekaserver/MsaEurekaServerApplication.java
@@ -2,12 +2,12 @@ package me.saro.example.msaeurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class MsaEurekaServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MsaEurekaServerApplication.class, args);
 	}
-
 }
